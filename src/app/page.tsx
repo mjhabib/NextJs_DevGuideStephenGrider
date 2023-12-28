4d03c2bce2edd534d4ae273c762d799a5e277753
MJ Habib <mj.habib4@gmail.com>
2023-12-28 09:53:22 +0330

    setting up authentication using auth-next & github OAuth in both server & client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import { Button } from "@nextui-org/react"
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>nextui button</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+      {session?.user ? (
+        <div>{session.user.email} Is Signed In</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+      {/* the '?' after session makes sure that the session object is defined, if not the expression after ':' will executed */}
+
+      <Profile />
     </div>
-  )
+  );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+// this provider will help us find out whether or not a user signed in from a client component
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
